2a4900b556b4f6bf543151c5034cb9f3e6e11564
tonycons <70544875+tonycons@users.noreply.github.com>
2026-03-11 09:39:54 -0400

    Temporary backup of my progress

--- backend/src/main/java/edu/gcc/ramraiders/Filter.java
+++ backend/src/main/java/edu/gcc/ramraiders/Filter.java
@@ -1,7 +1,10 @@
 package edu.gcc.ramraiders;
 
+import java.util.HashSet;
 import java.util.Set;
 import java.util.Date;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * @param creditHours Selects a course if its number of credit hours is in this set. e.g: [3, 4, 5]
@@ -13,14 +16,12 @@ import java.util.Date;
  * @param semesters Selects a course if it's in a given semester
  * @param years Selects a course if it's in a given year
  * @param codes Selects a course if it has a code (e.g. 101 returns all 101 courses)
- * @param keywords Select a course if any string in keywords is a substring of the course name
  *
  * @apiNote Any null value means that attribute is not being searched for
  */
 public record Filter(
         Set<String> departments,
         Set<Integer> codes,
-        Set<String> keywords,
         Set<Course.SemesterType> semesters,
         Set<Integer> years,
         Set<String> professors,
@@ -30,4 +31,22 @@ public record Filter(
         Set<Course> prerequisites
 ) {
     public record Timeslot(int hour, int minute, int length) {}
+
+    /**
+     * @return A new Filter which is this filter combined with another filter.
+     * @param other the other filter
+     */
+    public Filter combine(Filter other) {
+        return new Filter(
+                Stream.concat(departments.stream(), other.departments.stream()).collect(Collectors.toSet()),
+                Stream.concat(codes.stream(), other.codes.stream()).collect(Collectors.toSet()),
+                Stream.concat(semesters.stream(), other.semesters.stream()).collect(Collectors.toSet()),
+                Stream.concat(years.stream(), other.years.stream()).collect(Collectors.toSet()),
+                Stream.concat(professors.stream(), other.professors.stream()).collect(Collectors.toSet()),
+                Stream.concat(timeslots.stream(), other.timeslots.stream()).collect(Collectors.toSet()),
+                Stream.concat(creditHours.stream(), other.creditHours.stream()).collect(Collectors.toSet()),
+                isAvailable || other.isAvailable,
+                Stream.concat(prerequisites.stream(), other.prerequisites.stream()).collect(Collectors.toSet())
+        );
+    }
 }
--- backend/src/main/java/edu/gcc/ramraiders/Search.java
+++ backend/src/main/java/edu/gcc/ramraiders/Search.java
@@ -5,6 +5,13 @@ import java.util.*;
 public class Search {
 
     private Set<Course> results;
+    private Filter combinedFilter;
+    private String currentQuery;
+
+    public Search() {
+        results = new HashSet<>();
+        combinedFilter = null;
+    }
 
     /**
      * @return The courses selected by the current filter
@@ -13,10 +20,121 @@ public class Search {
         return results;
     }
 
+    private boolean courseMatchesByWordSet(List<String> words, Course course) {
+        for (int i = 0; i < words.size(); i++) {
+            String word = words.get(i);
+
+            // check for department
+            if (course.department().contains(word)) {
+                return true;
+            }
+            // check for code or year
+            try {
+                int codeOrYear = Integer.parseInt(word);
+                if (course.code() == codeOrYear || course.year() == codeOrYear) {
+                    return true;
+                }
+            } catch (NumberFormatException ignored) {
+            }
+            // check for section
+            if (course.section() == word.charAt(0)) {
+                return true;
+            }
+            // check for name
+            if (course.name().contains(word)) {
+                return true;
+            }
+
+            // check for semester
+            if (word.equalsIgnoreCase("fall") && course.semester() == Course.SemesterType.Fall) {
+                return true;
+            }
+            if (word.equalsIgnoreCase("spring") && course.semester() == Course.SemesterType.Spring) {
+                return true;
+            }
+            if (word.equalsIgnoreCase("summer") && (course.semester() == Course.SemesterType.EarlySummer
+                || course.semester() == Course.SemesterType.LateSummer)) {
+
+                boolean hasLate = i > 0 && words.get(i - 1).equalsIgnoreCase("late");
+                boolean hasEarly = i > 0 && words.get(i - 1).equalsIgnoreCase("early");
+
+                // edge case: ambiguous summer class specified
+                if (!hasLate && !hasEarly) {
+                    return true;
+                }
+                // late or early was specified
+                if (hasLate && course.semester() == Course.SemesterType.LateSummer) {
+                    return true;
+                }
+                else if (hasEarly && course.semester() == Course.SemesterType.EarlySummer) {
+                    return true;
+                }
+            }
+            if (word.equalsIgnoreCase("winter") && course.semester() == Course.SemesterType.WinterOnline) {
+                return true;
+            }
+
+            // check for professor names
+            if (course.professorNames().contains(word)) {
+                return true;
+            }
+
+            // check for meeting times
+            for (var meetingTime : course.meetingTimes()) {
+                if ((word.equalsIgnoreCase("monday") && meetingTime.day() == Course.Day.Monday)
+                || (word.equalsIgnoreCase("tuesday") && meetingTime.day() == Course.Day.Tuesday)
+                || (word.equalsIgnoreCase("wednesday") && meetingTime.day() == Course.Day.Wednesday)
+                || (word.equalsIgnoreCase("thursday") && meetingTime.day() == Course.Day.Thursday)
+                || (word.equalsIgnoreCase("friday") && meetingTime.day() == Course.Day.Friday)){
+                    return true;
+                }
+                try {
+                    var timeParts = word.split(":");
+                    int hr = Integer.parseInt(timeParts[0]);
+                    int min = Integer.parseInt(timeParts[1]);
+                    if (meetingTime.hour() == hr || meetingTime.minute() == min) {
+                        return true;
+                    }
+                } catch (NumberFormatException ignored) {}
+            }
+        }
+        return false;
+    }
+
     /**
      * Updates the search results' filter
      * @param filter The filter
      */
-    public void applyFilter(Filter filter) {
+    public void applyFilter(String query, Filter filter) {
+
+        // Reset results
+        if (!query.equals(currentQuery)) {
+            results.clear();
+            var words = Arrays.asList(query.split("\\s+"));
+            for (Course course : Main.courseDB.getCourseList()) {
+                if (courseMatchesByWordSet(words, course)) {
+                    results.add(course);
+                }
+            }
+        }
+        else {
+
+        }
+
+//        if (combinedFilter == null) {
+//            combinedFilter = filter;
+//        } else {
+//            combinedFilter = combinedFilter.combine(filter);
+//        }
+//
+//        // This is not the highest performance but make it work first
+//        results.clear();
+//        for (String dept : combinedFilter.departments()) {
+//            results.addAll(Main.courseDB.getCoursesByDepartment(dept));
+//        }
+//        for (int codes : combinedFilter.codes()) {
+//            results.addAll(Main.courseDB.getCoursesByCode(codes));
+//        }
+
     }
 }
