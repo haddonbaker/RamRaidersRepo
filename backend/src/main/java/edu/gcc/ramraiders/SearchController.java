29bd4d2ce0bf2411b76163aed8eaa6d0c50cd26b
BLANKSJM20 <BlanksJM20@gcc.edu>
2026-04-22 13:41:54 -0400

    added an import

--- backend/src/main/java/edu/gcc/ramraiders/SearchController.java
+++ backend/src/main/java/edu/gcc/ramraiders/SearchController.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import edu.gcc.ramraiders.EmailUtil;
 
 public class SearchController {
     public static Search search;
